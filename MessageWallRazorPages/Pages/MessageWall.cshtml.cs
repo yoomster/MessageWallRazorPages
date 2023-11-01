bb76b4f684a3480c557b256145026127070566f9
naomifleurnoel <nperenboom@hotmail.com>
2023-11-01 21:44:46 +0100

    Add project files.

---
+++ MessageWallRazorPages/Pages/MessageWall.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MessageWallRazorPages.Pages
+{
+    public class MessageWallModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
