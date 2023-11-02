c258a8facd3116bb3c40d3ed0ca7c5894b31feb5
naomifleurnoel <nperenboom@hotmail.com>
2023-11-02 16:07:49 +0100

    Added the page + routing for the message wall = stateless

--- MessageWallRazorPages/Pages/MessageWall.cshtml.cs
+++ MessageWallRazorPages/Pages/MessageWall.cshtml.cs
@@ -5,9 +5,23 @@ namespace MessageWallRazorPages.Pages
 {
     public class MessageWallModel : PageModel
     {
+        [BindProperty]
+        public List<string> Messages { get; set; } = new List<string>();
+
+        [BindProperty]
+        public string Message { get; set; } 
+
         public void OnGet()
         {
+            //eventually this will read from the database
+        }
 
+        public IActionResult OnPost()
+        {
+            //instead of adding to this list - it eventually will post to database!
+            Messages.Add(Message);
+
+            return Page();
         }
     }
 }
